38aada018ab2629ef53bb6dfd33c8484acee3f21
Seonghwan Seo <thespeace1@gmail.com>
2025-05-16 17:56:39 +0900

    Time: 1 ms (38.73%), Space: 43.2 MB (63.16%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]); // 마지막 계단은 반드시 밟지 않아도 됨
+    }
+}
