c69a8e385cea2583508aa9d51e5fead2b3144d39
Seonghwan Seo <thespeace1@gmail.com>
2025-05-21 17:50:11 +0900

    Time: 1 ms (62.11%), Space: 41.8 MB (61.4%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for (char ch : jewels.toCharArray()) {
+            jewelSet.add(ch);
+        }
+
+        int count = 0;
+        for (char ch : stones.toCharArray()) {
+            if (jewelSet.contains(ch)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
