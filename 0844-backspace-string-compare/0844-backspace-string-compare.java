bdd2dab3c13566e4c88d23cad71129b2f096a2c8
Seonghwan Seo <thespeace1@gmail.com>
2025-06-03 22:43:06 +0900

    Time: 2 ms (49.15%), Space: 41.6 MB (66.55%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    private String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '#') {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) sb.append(c);
+        return sb.toString();
+    }
+}
